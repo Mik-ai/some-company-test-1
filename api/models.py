60fbafb4a63b832a12ab461efbe63a9c6cb2f655
Mikhail Lysakov <mishas535@gmail.com>
2023-06-28 11:40:58 +0300

    mend

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
-class User(models.Model):
+class UserData(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
 
