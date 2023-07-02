057103285eaa2ff7ca9f2eff0030d743b5406637
Mikhail Lysakov <mishas535@gmail.com>
2023-07-02 12:24:48 +0300

    change crutch to normal solution

--- api/filters.py
+++ api/filters.py
@@ -5,7 +5,6 @@ from django_filters.rest_framework import (
     FilterSet,
     CharFilter,
 )
-from .models import UserData
 
 GenderChoices = (
     ("Male", "Male"),
@@ -13,24 +12,34 @@ GenderChoices = (
 )
 
 
+class MyBackend(DjangoFilterBackend):
+    def get_filterset_kwargs(self, request, queryset, view):
+        kwargs = super().get_filterset_kwargs(request, queryset, view)
+
+        # merge filterset kwargs provided by view class
+        if hasattr(view, "passing_context"):
+            kwargs.update(view.passing_context())
+
+        return kwargs
+
+
 class UserDataFilterSet(FilterSet):
     name = CharFilter()
     surname = CharFilter()
     gender = ChoiceFilter(choices=GenderChoices)
-    id = NumberFilter(method="id_filter")
     distance = NumberFilter(method="distance_filter", lookup_expr="some")
 
-    # when u don't have time but work should be done...
-    def id_filter(self, queryset, *args, **kwargs):
-        return queryset
+    def __init__(self, *args, active_user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.active_user = active_user
 
     def distance_filter(self, queryset, *args, **kwargs):
         distance = self.data.get("distance")
         distance = float(distance)
 
-        user = UserData.objects.get(pk=self.data.get("id"))
-
-        filtered_ids = [x.id for x in queryset if user.distance_to(x) < distance]
-        queryset = UserData.objects.filter(id__in=filtered_ids)
+        filtered_ids = [
+            x.id for x in queryset if self.active_user.distance_to(x) < distance
+        ]
+        queryset = queryset.filter(id__in=filtered_ids)
 
         return queryset
--- api/views.py
+++ api/views.py
@@ -1,9 +1,7 @@
 from rest_framework import viewsets, permissions, status, generics
 from rest_framework.response import Response
 
-from django_filters.rest_framework import DjangoFilterBackend, NumberFilter
-
-from .filters import UserDataFilterSet
+from .filters import UserDataFilterSet, MyBackend
 from .models import UserData
 from .serializers import UserSerializer, FollowUserSerializer, UserAllfieldsSerializer
 
@@ -28,8 +26,6 @@ class FollowUser(viewsets.ViewSet):
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
         follow = request.data.get("follow")
-        # todo - удалить дебаг...
-        print(f"{instance.id} - {follow}")
 
         # существует ли пользователь на которого подписываемся?
         if not UserData.objects.filter(id=follow).exists():
@@ -62,14 +58,11 @@ class FollowUser(viewsets.ViewSet):
 class UserList(generics.ListAPIView):
     queryset = UserData.objects.all()
     serializer_class = UserAllfieldsSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [MyBackend]
     filterset_class = UserDataFilterSet
     exclude = ["id"]
 
-    # distance = NumberFilter(method="distance_filter")
-    # def distance_filter(self, distance):
-    #     user = self.context["request"].user
-    #     user = UserData.objects.get(user=user)
-    #     queryset = UserData.objects.all()
-    #     queryset = [x for x in queryset if user.distance_to(x) < distance]
-    #     return queryset
+    def passing_context(self):
+        return {
+            'active_user': UserData.objects.get(user=self.request.user),
+        }
